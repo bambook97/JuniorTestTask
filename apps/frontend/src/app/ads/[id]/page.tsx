a307e453e4902cb62410bfdb69e98a8a4c91f778
Nikita Voropaev <bambook97@gmail.com>
2024-03-04 04:05:25 +0600

    Feat: add adView widget

--- apps/frontend/src/app/ads/[id]/page.tsx
+++ apps/frontend/src/app/ads/[id]/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import React from 'react';
+import AdViewSkeleton from '../../../widgets/AdView/skeleton/AdViewSkeleton';
 import useFetchAdData from '../../../shared/hooks/useFetchAdData';
 import AdView from '../../../widgets/AdView/AdView';
 
 export default function AdPage({ params }: { params: { id: string } }) {
-  const { data } = useFetchAdData({ id: params.id });
-  if (!data) return null;
+  // Работал в основном с PageRouter и получал данные через getServerSideProps, почитал в доке как фетчить данные в SSR с AppRouer,
+  // но получал ошибку подключения к api, сделал обычным клиентским компонентом
 
-  return (
-    <div>
-      <AdView data={data} />
-    </div>
-  );
+  const { data } = useFetchAdData({ id: params.id });
+
+  if (!data) return <AdViewSkeleton />;
+
+  return <AdView data={data} />;
 }
--- apps/frontend/src/widgets/AdView/AdView.tsx
+++ apps/frontend/src/widgets/AdView/AdView.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import Image from 'next/image';
 import { Ads } from '../../shared/types/types';
 import Carousel from '../../shared/ui/Carousel/Carousel';
+import LikeButton from '../../features/AdsCard/LikeButton/LikeButton';
+import style from './adView.module.scss';
 
-interface Props {
+interface AdViewProps {
   data: Ads;
 }
-const AdView = ({ data }: Props) => {
-  console.log(data);
-  return (
-    <div>
-      <Carousel images={data.images} />
+const AdView = ({ data }: AdViewProps) => (
+  <div className={style.wrapper}>
+    <Carousel images={data.images} />
+    <div className={style.textBlock}>
+      <div className={style.productInfo}>
+        <h3>{data.title}</h3>
+        <LikeButton id={data.id} />
+      </div>
+      <div className={style.productInfo}>
+        <h3>
+          {data.city_name},{data.district_name}
+        </h3>
+        <h3>{data.price}</h3>
+      </div>
+      <h3 className={style.description}>{data.description}</h3>
     </div>
-  );
-};
+  </div>
+);
 
 export default AdView;
